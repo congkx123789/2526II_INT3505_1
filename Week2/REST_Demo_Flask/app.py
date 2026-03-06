2bfe6e0ffbd55ea6bd0e2ec6531e76c3e7028309
cong <havucong25@gmail.com>
2026-03-06 09:07:55 +0700

    cacheable

--- Week2/REST_Demo_Flask/app.py
+++ Week2/REST_Demo_Flask/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 import jwt
+import json
+import hashlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_super_secret_key'
@@ -42,9 +44,6 @@ def add_product():
 # ==========================================================
 @app.route('/api/login', methods=['POST'])
 def login():
-    """
-    Client lấy Token. Server KHÔNG LƯU session trong RAM hay ổ cứng.
-    """
     data = request.json
     if data and data.get('username') == 'admin':
         token = jwt.encode({'user': 'admin'}, app.config['SECRET_KEY'], algorithm='HS256')
@@ -53,21 +52,43 @@ def login():
 
 @app.route('/api/secure-data', methods=['GET'])
 def secure_data():
-    """
-    Client BẮT BUỘC gửi kèm Token ở mọi Request nếu muốn pass qua cửa này.
-    Server tự giải nghĩa Token chứ Server không tra Session từ Database.
-    Hệ thống lúc này có Scale 10 máy lên cũng không sập vì chia sẻ Session dễ dàng.
-    """
     token = request.headers.get('Authorization')
     if not token:
         return jsonify({'message': 'Token is missing!'}), 401
-    
     try:
-        token = token.split(" ")[1] # Xóa chữ Bearer
+        token = token.split(" ")[1] 
         decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
         return jsonify({'message': 'Welcome admin!', 'secret_data': 'Phi trạng thái có nghĩa là Server quên bạn ngay sau khi gửi xong Request.'})
     except Exception as e:
         return jsonify({'message': 'Token is invalid!', 'error': str(e)}), 401
+
+# ==========================================================
+# 4. Cacheable
+# ==========================================================
+@app.route('/api/product/<int:product_id>', methods=['GET'])
+def get_product_etag(product_id):
+    """
+    Sử dụng ETag để tiết kiệm băng thông khi Data chưa bị sờ tới.
+    """
+    product = next((p for p in db_products if p["id"] == product_id), None)
+    if not product:
+        return jsonify({"message": "Not Found"}), 404
+        
+    # Tạo mã băm ETAG 
+    product_str = json.dumps(product, sort_keys=True).encode('utf-8')
+    etag_hash = hashlib.md5(product_str).hexdigest()
+    
+    # Kiểm tra ETAG trong HTTP Headers Browser nếu nó quay lại
+    if_none_match = request.headers.get('If-None-Match')
+    
+    if if_none_match and if_none_match.strip('"') == etag_hash:
+        # Nếu Data Database trùng khớp mã ETag của Client gửi lên (100% Data giống nhau)
+        # Server ngắt kết nối luôn và trả 304 - Băng thông siêu thấp
+        return '', 304 
+
+    response = make_response(jsonify(product))
+    response.set_etag(etag_hash)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
